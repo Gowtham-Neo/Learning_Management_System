fe1e71226dcee80c0870916ee02a0058f2dab35e
Gowtham-Neo <s.gowtham.engr@gmail.com>
2023-11-03 23:47:36 +0530

    Added test case

---
+++ __tests__/lmsTest.js
@@ -0,0 +1,71 @@
+const request = require("supertest");
+const cheerio = require("cheerio");
+const db = require("../models/index");
+const app = require("../app");
+let server, agent;
+
+function extractCsrfToken(response) {
+  const $ = cheerio.load(response.text);
+  return $("[name=_csrf]").val();
+}
+
+const login = async (agent, username, password) => {
+  let response = await agent.get("/signin/educator");
+  let csrfToken = extractCsrfToken(response);
+  response = await agent.post("/educator/login").send({
+    email: username,
+    password: password,
+    _csrf: csrfToken,
+  });
+};
+
+describe("Learning Management System test suite", () => {
+  beforeAll(async () => {
+    await db.sequelize.sync({ force: true });
+    server = app.listen(3000, () => {});
+    agent = request.agent(server);
+  });
+
+  afterAll(async () => {
+    await db.sequelize.close();
+    server.close();
+  });
+
+  test("educator signup", async () => {
+    let res = await agent.get("/educator/signup");
+    const csrfToken = extractCsrfToken(res);
+    res = await agent.post("/educator/signup").send({
+      firstName: "Test",
+      lastName: "User",
+      email: "user@test.com",
+      password: "helloworld1234",
+      _csrf: csrfToken,
+    });
+  });
+
+  test("Sign up as Student", async () => {
+    let res = await agent.get("/signup");
+    const csrfToken = extractCsrfToken(res);
+    res = await agent.post("/student/signup").send({
+      firstName: "student",
+      lastName: "1",
+      email: "student1@test.com",
+      password: "password1234",
+      role: "Student",
+      _csrf: csrfToken,
+    });
+  });
+
+  test("Creates a Course", async () => {
+    const agent = request.agent(server);
+    await login(agent, "user@test.com", "helloworld1234");
+    const res = await agent.get("/course");
+    const csrfToken = extractCsrfToken(res);
+    await agent.post("/course").send({
+      title: "Web Development",
+      educatorName: "Test",
+      EducatorId: 1,
+      _csrf: csrfToken,
+    });
+  });
+});
